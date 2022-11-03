b0c89d2bf675f87fdac1c761ab4e73cc07b499b9
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2022-11-03 10:09:27 -0500

    finishing all labs in fundamentals

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -2,20 +2,42 @@ package labs_examples.fundamentals.labs;
 
 /**
  * Fundamentals Exercise 4: My Second Program
- *
- *      Write the necessary code to display, declare and print each of Java's primitive data types.
- *      Please declare a single variable of each type, give it an appropriate value, then print that variable.
- *
+ * <p>
+ * Write the necessary code to display, declare and print each of Java's primitive data types.
+ * Please declare a single variable of each type, give it an appropriate value, then print that variable.
  */
 
 public class Exercise_04 {
 
     public static void main(String[] args) {
 
-        int i = 1;
-        System.out.println("int i is: " + i);
+        // char
+        char letter = 'A';
+        System.out.println("letter = " + letter);
 
-        // write your code below
+        // byte
+        byte b = 127;
+        System.out.println("byte = " + b);
+
+        // short
+        short shortVal = 32767;
+        System.out.println("short = " + shortVal);
+
+        // int
+        int intVal = 2147;
+        System.out.println("integer = " + intVal);
+
+        // long
+        long longVal = 1234123412341234L;
+        System.out.println("long = " + longVal);
+
+        // float
+        float floatVal = 41234.12341234f;
+        System.out.println("float = " + floatVal);
+
+        // double
+        double doubleVal = 1234.1234;
+        System.out.println("double = " + doubleVal);
 
     }
 

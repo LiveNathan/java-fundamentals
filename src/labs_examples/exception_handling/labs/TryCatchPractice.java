04a00b116f91ca6f6b6d6c7ab75abc3f08072916
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2022-11-28 12:05:38 -0600

    learning about exceptions

---
+++ src/labs_examples/exception_handling/labs/TryCatchPractice.java
@@ -0,0 +1,29 @@
+package labs_examples.exception_handling.labs;
+
+public class TryCatchPractice {
+    /*
+    1) Run this program as is. Do you see the exception thrown? Does "Program Complete" print?
+
+    2) Uncomment the try/catch block (the 4 lines that start with "//") then run the application again. See the difference? Did "Program complete" print?
+
+    3) Try to generate a new exception and catch it properly.
+*/
+
+    public static void main(String[] args) {
+
+        int[] nums = {67, 43};
+
+        try {
+            System.out.println(nums[5]);
+        } catch (ArrayIndexOutOfBoundsException exc) {
+            System.out.println("Error detected. Sorry bout that!");
+        }
+        try {
+            int a = nums[0] / 0;
+        } catch (ArithmeticException exception){
+            System.out.println("You can't divide by zero.");
+        }
+        System.out.println("Program complete");
+    }
+}
+
---
+++ src/labs_examples/exception_handling/labs/playgroundDivisionByZero.java
@@ -0,0 +1,19 @@
+package labs_examples.exception_handling.labs;
+
+public class playgroundDivisionByZero {
+    public int division(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw ArithmeticException;
+        } else {
+            return a / b;
+        }
+
+
+
+//        try {
+//            return a / b;
+//        } catch (ArithmeticException arithmeticException) {
+//            return -1;
+//        }
+    }
+}
